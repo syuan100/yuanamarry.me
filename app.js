8d0bf138e87103d6424c94e8e788672a8bdc6466
syuan100 <syuan100@gmail.com>
2015-12-16 10:05:54 -0800

    New mysql connection code

--- app.js
+++ app.js
@@ -9,16 +9,33 @@ var connection = mysql.createConnection({
   password : 'callwetip1'
 });
 
-connection.query('USE test_database');
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+
+  connection.query('CREATE TABLE people(id int primary key, name varchar(255), age int, address text)', function(err, result) {
+    if (err) throw err
+    connection.query('INSERT INTO people (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA'], function(err, result) {
+      if (err) throw err
+      connection.query('SELECT * FROM people', function(err, results) {
+        if (err) throw err
+        console.log(results[0].id)
+        console.log(results[0].name)
+        console.log(results[0].age)
+        console.log(results[0].address)
+      })
+    })
+  }) 
+})
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-  connection.query('SELECT * FROM users', function(err, rows){
-    res.render('users', {users : rows});
-  });
-});
+// app.get('/', function(req, res){
+//   connection.query('SELECT * FROM users', function(err, rows){
+//     res.render('users', {users : rows});
+//   });
+// });
 
 module.exports = app;
