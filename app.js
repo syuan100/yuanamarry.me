56db39af91b5698c9d56b4dd58df84f2521dbff3
syuan100 <syuan100@gmail.com>
2015-12-16 10:11:38 -0800

    Comment out auto table creation

--- app.js
+++ app.js
@@ -10,24 +10,24 @@ var connection = mysql.createConnection({
   database : 'test_database'
 });
 
-connection.connect(function(err) {
-  if (err) throw err
-  console.log('You are now connected...')
+// connection.connect(function(err) {
+//   if (err) throw err
+//   console.log('You are now connected...')
 
-  connection.query('CREATE TABLE people(id int primary key, name varchar(255), age int, address text)', function(err, result) {
-    if (err) throw err
-    connection.query('INSERT INTO people (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA'], function(err, result) {
-      if (err) throw err
-      connection.query('SELECT * FROM people', function(err, results) {
-        if (err) throw err
-        console.log(results[0].id)
-        console.log(results[0].name)
-        console.log(results[0].age)
-        console.log(results[0].address)
-      })
-    })
-  }) 
-})
+//   connection.query('CREATE TABLE people(id int primary key, name varchar(255), age int, address text)', function(err, result) {
+//     if (err) throw err
+//     connection.query('INSERT INTO people (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA'], function(err, result) {
+//       if (err) throw err
+//       connection.query('SELECT * FROM people', function(err, results) {
+//         if (err) throw err
+//         console.log(results[0].id)
+//         console.log(results[0].name)
+//         console.log(results[0].age)
+//         console.log(results[0].address)
+//       })
+//     })
+//   }) 
+// })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
