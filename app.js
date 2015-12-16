c9e1e33960488754ce255bfa01ed581d826de63f
syuan100 <syuan100@gmail.com>
2015-12-16 09:56:25 -0800

    Try mysql

--- app.js
+++ app.js
@@ -1,59 +1,26 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var mysql = require('mysql');
 var app = express();
 
-// view engine setup
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'callwetip1'
+});
+
+connection.query('USE test_database');
+
+app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function(req, res){
+  connection.query('SELECT * FROM users', function(err, rows){
+    res.render('users', {users : rows});
+  });
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+app.listen(app.get('port'));
+console.log('Express server listening on port ' + app.get('port'));
