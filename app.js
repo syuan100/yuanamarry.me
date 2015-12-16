e14dd1e7be373584d852c8211f9b616dfe889ef1
syuan100 <syuan100@gmail.com>
2015-12-16 11:07:18 -0800

    Add connection code

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var connection = mysql.createConnection({
   database : 'test_database'
 });
 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
