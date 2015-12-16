145906e9e034547f089cf3928cc614bf3ff36d77
syuan100 <syuan100@gmail.com>
2015-12-16 10:19:38 -0800

    Change key for user object in tempalte

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  console.log("recieved");
   connection.query('SELECT * FROM people', function(err, rows){
     res.render('index', {users : rows});
   });
