d0b5ea0fec22b925942f5cf65848960d17130707
syuan100 <syuan100@gmail.com>
2015-12-16 18:04:41 -0800

    Typo fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/stylesheets*', function(req, res) {
-  console.log("got something");/
+  console.log("got something");
 });
 
 module.exports = app;
