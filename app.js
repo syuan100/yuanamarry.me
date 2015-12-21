8a49802f40a164195eb3e7a957ddab1224c347af
syuan100 <syuan100@gmail.com>
2015-12-20 21:05:07 -0800

    Use new path

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var app = express();
 // This example starts a SMTP server using TLS with your own certificate and key
 var server = new SMTPServer({
     secure: true,
-    key: fs.readFileSync('sec/domain.key'),
-    cert: fs.readFileSync('sec/domain.crt')
+    key: fs.readFileSync(path.join(__dirname, 'sec', 'domain.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'sec', 'domain.crt'))
 });
 server.listen(465);
 
