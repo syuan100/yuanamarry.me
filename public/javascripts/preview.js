ca78f2e61de3520a728e9f20563d310c70a37367
syuan100 <syuan100@gmail.com>
2015-12-21 14:50:58 -0800

    Try ajax for api

--- public/javascripts/preview.js
+++ public/javascripts/preview.js
@@ -1,7 +1,22 @@
+var populateEmailList = function(data){
+  console.log(data);
+};
+
 $(document).ready(function(){
   /* initial load */
   $("input[value='general']").click();
 
   /* populate select field */
-  
+  $.ajax({
+    url: "/admin/api/sendees",
+    success: function(data) {
+      populateEmailList(data);
+    },
+    error: function(xhr, status, error) {
+      console.log(xhr);
+      console.log(status);
+      console.log(error);
+    }
+  });
+
 });
