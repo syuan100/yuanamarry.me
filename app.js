22e5eb139def9442ffc008ec5d98c658d5a06b88
syuan100 <syuan100@gmail.com>
2015-12-22 22:47:17 -0800

    Update tracker

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ app.get('/tracker/*',function(req,res){
   var trackerArray = base32.decode(req.path.split('.')[0].split('\/tracker\/')[1]).split("|");
   var now = new Date();
   var timeString = moment(now).tz("America/Los_Angeles").format("M/DD hA") + "<br />Opened"
-
+  console.log(trackerArray);
   if((trackerArray[1] == "std") || (trackerArray[1] == "invitation")){
     var trackingQuery = "UPDATE people SET " + trackerArray[1] + " = '" + timeString + "' WHERE email = '" + trackerArray[0] + "';";
     connection.query(trackingQuery, function(err, result) {
