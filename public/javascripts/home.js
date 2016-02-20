a53e6e37035269e7e8ec5e3d8353dbffa207851a
Steven Yuan <syuan100@gmail.com>
2016-02-20 13:10:28 -0800

    js for close button

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   var $rsvpButton = $('.rsvp-action');
   var $rsvpWindow = $('.rsvp-window');
+  var $closeButton = $('.rsvp-window .close-button');
 
   $rsvpButton.click(function() {
     $rsvpWindow.fadeIn();
   });
+
+  $closeButton.click(function(){
+    $rsvpWindow.fadeOut();
+  });
+
 });
