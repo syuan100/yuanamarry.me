57ac9e4ff86ece24804929db4017b6e81f672c4e
Steven Yuan <syuan100@gmail.com>
2016-02-20 13:14:19 -0800

    Update css for header & js for focusing on email

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   var $rsvpButton = $('.rsvp-action');
   var $rsvpWindow = $('.rsvp-window');
   var $closeButton = $('.rsvp-window .close-button');
+  var $emailField = $('input.email');
+  var $passField = $('input.passcode');
 
   $rsvpButton.click(function() {
-    $rsvpWindow.fadeIn();
+    $rsvpWindow.fadeIn(fucntion() {
+      $emailField.focus();
+    });
   });
 
   $closeButton.click(function(){
