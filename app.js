889ecc147b796a804d14b3753a626bfc46167e97
Steven Yuan <syuan100@gmail.com>
2016-02-20 23:54:01 -0800

    Typo

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ app.get('/admin/db/fix', auth, function(req, res){
       if (result) {
         var check = JSON.stringify({ data: result });
         var exists = JSON.parse(check).data[0][Object.keys(JSON.parse(check).data[0])];
-        console.log(exists)
+        console.log(exists);
         // if (!exists){
         //   console.log(desiredfields[i] + " doesn't exist");
         // }
