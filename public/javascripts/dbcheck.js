0390a5526d64c5840f2f208da601fc9211a23537
Steven Yuan <syuan100@gmail.com>
2016-02-20 21:11:27 -0800

    Update the selection

--- public/javascripts/dbcheck.js
+++ public/javascripts/dbcheck.js
@@ -1,11 +1,10 @@
 function matchFields(){
   var matches = $(".desired-schema .matches td");
   var $current = $(".current-schema");
-  console.log(matches);
 
   $.each(matches, function(i,e){
-    console.log(e);
-    if($current.find(e).length) {
+    var currentClass = $(e).attr('class');
+    if($current.find(currentClass).length) {
       $(".desired-schema").find(e).text("TRUE");
     } else {
       $(".desired-schema").find(e).text("FALSE");
