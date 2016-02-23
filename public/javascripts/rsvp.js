48d686124fab14d1f7f3e01c4eb66dea2641b51c
Steven Yuan <syuan100@gmail.com>
2016-02-23 15:51:16 -0800

    Update additional guests script

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -3,6 +3,8 @@ function additionalGuests(n) {
   for(var i=0; i<(n + 1); i++){
     $guestBox.append("<option value='"+ i +"'>" + i + "</option>");
   }
+  if($guestBox.find("option").length)
+    $(".additional-guests").show();
 };
 
 $(document).ready(function() {
