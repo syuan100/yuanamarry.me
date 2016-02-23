f7ecf2a79b9f07a573d15fced734c37fec1f82b2
Steven Yuan <syuan100@gmail.com>
2016-02-23 15:49:37 -0800

    Additional counter

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -1,6 +1,6 @@
 function additionalGuests(n) {
   var $guestBox = $(".additional-guests select");
-  for(var i=0; i<n; i++){
+  for(var i=0; i<(n + 1); i++){
     $guestBox.append("<option value='"+ i +"'>" + i + "</option>");
   }
 };
