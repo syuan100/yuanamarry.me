cf5ad9dee02757ddc0fbd4d1a6f05030a383e8bd
Steven Yuan <syuan100@gmail.com>
2016-02-28 21:45:25 -0800

    Test set rsvp function

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -17,19 +17,38 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+function addRsvpForms(number) {
+  $(".form.meal-selection").empty();
+  for(var j=0; j < (number + 1); j++) {
+    $(".form.meal-selection").append($mealForm);
+  }
+  for(var k=0; k < ($(".meal-form").length); k++) {
+    $.each($($(".meal-form")[k]).find(".meal-selection-radio"), function(i,e){
+      $(e).attr("name", "meal-selection-" + k);
+    });
+  }
+}
+
+function setRsvp(rsvp, additional_guests, meal_choices){
+  $("input.attendance-choice[value='" + choice + "']").prop("checked", true);
+  $(".additional-guests select option[value='" + additional_guests + "']").prop("selected", true);
+  addRsvpForms(additional_guests);
+  var mealPeople = meal_choices.split('&');
+
+  for(var k=0; k < ($(".meal-form").length); k++) {
+    var mealName = mealPeople[k].split("|")[0];
+    var mealChoice = mealPeople[k].split("|")[1];
+    $($(".meal-form")[k]).find(".meal-name").text();
+    $($(".meal-form")[k]).find("selection[value='" + mealChoice + "'").prop("selected", true);
+  }
+
+}
+
 $(document).ready(function() {
   var $mealForm = $(".meal-form")[0].outerHTML;
   $(".additional-guests select").change(function(){
     var additionalSelections = $(this).val();
-    $(".form.meal-selection").empty();
-    for(var j=0; j < (parseInt(additionalSelections) + 1); j++) {
-      $(".form.meal-selection").append($mealForm);
-    }
-    for(var k=0; k < ($(".meal-form").length); k++) {
-      $.each($($(".meal-form")[k]).find(".meal-selection-radio"), function(i,e){
-        $(e).attr("name", "meal-selection-" + k);
-      });
-    }
+    addRsvpForms(additionalSelections);
   });
 
   $(".rsvp-submit").click(function(){
