5c0fd98e9c6f20bac9d15fe049ce8f08d23cef04
Steven Yuan <syuan100@gmail.com>
2016-03-02 22:49:04 -0800

    log

--- app.js
+++ app.js
@@ -463,7 +463,9 @@ app.post('/rsvp-submit', function(req, res){
 
 app.post('/admin/db/set_additional_guests', auth, function(req,res){
   var updateObject = req.body;
-  console.log(req.body);
+  console.log(JSON.parse(req.body));
+  console.log(JSON.parse(req.body)[0]);
+  console.log(JSON.parse(JSON.parse(req.body)[0]));
   for(var i=0; i<updateObject.length; i++){
     var rowObject = updateObject[i.toString()];
     var rowEmail = rowObject.email;
@@ -473,7 +475,6 @@ app.post('/admin/db/set_additional_guests', auth, function(req,res){
       rowGuests = 0;
     }
     var rowUpdateQuery = "UDPATE people SET additional_spots='" + rowGuests + "' WHERE email='" + rowEmail +"';";
-    console.log(rowUpdateQuery);
   }
   res.status(200).json({ success: "success"});
 });
