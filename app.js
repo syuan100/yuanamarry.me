763c4ec8e09fc968625b2f0578ff17560d8635fd
Steven Yuan <syuan100@gmail.com>
2016-03-02 22:36:38 -0800

    log

--- app.js
+++ app.js
@@ -463,6 +463,7 @@ app.post('/rsvp-submit', function(req, res){
 
 app.post('/admin/db/set_additional_guests', auth, function(req,res){
   var updateObject = JSON.parse(JSON.stringify(req.body));
+  console.log(updateObject.length);
   for(var i=0; i<updateObject.length; i++){
     var rowObject = updateObject[i.toString()];
     var rowEmail = rowObject.email;
